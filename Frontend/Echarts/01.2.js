57aff0ca2874c372fc4c6bb79935f9704b8a51fd
summetreechina <1095909657@qq.com>
2017-06-03 12:24:51 +0800

    Echarts
    Charts 很有意思 很方便
    也有一些莫名其妙的地方

---
+++ Frontend/Echarts/01.2.js
@@ -0,0 +1,11 @@
+>>> 浏览器画图原理
+	'https://github.com/shengxinjing/imooc-echarts'
+	1. Canvas 
+	-- 基于像素
+	-- 单个html 类似于画笔在画布上画画
+	-- Echarts 基于 Canvas 画图
+	2. Svg
+	-- 基于对象模型
+	-- 多个图形元素
+	-- 高保真
+'http://echarts.baidu.com/'
---
+++ Tool/Real-time_visual/01.1.js
@@ -0,0 +1,5 @@
+>>> 前端实时可视化工具介绍 'http://www.imooc.com/learn/718'
+
+	1. liveStyle	liveStyle.io
+	2. liveReload
+	3. broswer-sync
