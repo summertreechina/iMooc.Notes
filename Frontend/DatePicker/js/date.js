91a8e57eeabf1ec6c964653a87a35654f24fd7c5
summetreechina <1095909657@qq.com>
2017-06-13 15:02:37 +0800

    datePicker

--- Frontend/DatePicker/js/date.js
+++ Frontend/DatePicker/js/date.js
@@ -7,15 +7,30 @@ function DatePicker() {
 // DatePicker.prototype.
 
 let now = new Date();
+// Tue Jun 13 2017 14:11:20 GMT+0800 (CST) {}
 let curr_day = now.toLocaleDateString();
+// "2017-6-13"
 let curr_year = now.getFullYear();
+// curr_year = 2017
 let curr_month = now.getMonth() + 1;
+// curr_month = 6
 let curr_date = now.getDate();
+// curr_date = 13
+let curr_week = now.getDay();
+// 2
 let timestamp = now.getTime();
+// timestamp = 1497334280876
 
 let firstDay = new Date(curr_year, curr_month-1, 1);
-let firstDayWeek = firstDay.getDay();
-console.log(timestamp);
+// 6月1日 数据
+let firstDayOfWeek = firstDay.getDay();
+// 4
+let lastDay = new Date(curr_year, curr_month, 0);
+// 30 本月的最后一天 
+// 因为本月最后一天我并不知道 只能通过下一月的第一天的前一天来确认
+
+
+console.log();
 
 
 
@@ -100,3 +115,13 @@ $.fn.extend({
 });
 
 })(jQuery);
+
+
+
+
+
+
+
+
+
+
