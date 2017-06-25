5c174785d9a77cc03dfad7a6dabbab8cb24cc13f
summetreechina <1095909657@qq.com>
2017-06-25 15:26:37 +0800

    img

--- Frontend/plug_transform/js/main.js
+++ Frontend/plug_transform/js/main.js
@@ -9,10 +9,15 @@ let loopPlayInit =
 	let $imgList  = null;
 	let origin = ['125px', '800px'];
 	let imgArr = [
-		[],
-		[],
-		[],
+		['./images/1/1273111319680123879.jpg', './images/1/1564156445598718097.jpg', './images/1/27584547735220719.jpg', './images/1/731271989594446829.jpg'],
+		['./images/2/IMG_3499.JPG', './images/2/IMG_3639.JPG', './images/2/IMG_3645.JPG', './images/2/mmexport1438439699817.jpg'],
+		['./images/3/IMG_2876.JPG', './images/3/IMG_2877.JPG', './images/3/IMG_2879.JPG', './images/3/IMG_2888.JPG']
 	];
+	let imgGroupId = 0;
+	let imgAng = 45;
+
+	imgArr = creatImage(imgArr);
+
 
 	function init() {
 		$leftBtn  = $('.left-btn');
@@ -27,11 +32,12 @@ let loopPlayInit =
 	function configer() {
 		let ang = 10;
 		let aInit = -15;
-		$imgList.animate({origin: '125px'}, 1200)
+		$imgList.animate({origin: '125px'})
 		$imgList.each(function(id, el) {
 			$(el).animate({ rotate: aInit + (ang*id) + "deg"});
 		});
 	}
+
 	function setEvent() {
 		// $mainBox.onclick();
 		let mainBox = document.querySelector('.mainBox');
@@ -48,11 +54,29 @@ let loopPlayInit =
 			}
 		}
 	}
-	function anigo() {
 
+	function anigo(d) {
+		imgGroupId += d;
+		$imgList.each(function(i, el) {
+			let $currImg = $(el).children('img');
+			let $newImg = $(imgArr[imgGroupId][i]);
+			$(el).empty().append($newImg);
+			$(el).animate({ origin: '80' });
+			$(el).animate({ origin: '80', rotate: (0-d)*imgAng + 'deg' });
+		});
 	}
 	function creatImage(arr) {
-		return imgObjs;
+		let imgArr = [];
+		// 学了一招 循环
+		for(let i in arr) {
+			imgArr[i] = [];
+			for(let k in arr[i]) {
+				imgArr[i][k] = new Image();
+				imgArr[i][k].src = arr[i][k];
+			}
+		}
+
+		return imgArr;
 	}
 
 
