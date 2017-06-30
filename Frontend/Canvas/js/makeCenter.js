54bdec55e93da0de11c6f18130a4fd2ca833550e
summetreechina <1095909657@qq.com>
2017-06-30 11:36:28 +0800

    makeCenter

--- Frontend/Canvas/js/makeCenter.js
+++ Frontend/Canvas/js/makeCenter.js
@@ -1,16 +1,44 @@
 +function(){
 	'use strict';
 
-	function makeCenter() {
-		alert('')
+	let MakeCenter = function(settings, dom) {
+		let self = this;
+		self.dom = dom;
+		self.parent = dom.parent();
+
+
+		let topVal = (window.innerHeight - self.dom.height())/2;
+		topVal = (topVal > 0) ? topVal : 0;
+
+		self.parent.css('position', 'relative');
+		self.dom.css({
+			background: '#b5b5b5',
+			position: 'relative',
+			top : topVal
+		});
+
+		window.onresize = function(event) {
+			let topVal = (window.innerHeight - self.dom.height())/2;
+			self.dom.css({
+				background: '#b5b5b5',
+				position: 'relative',
+				top : topVal
+			});
+		};
+
+
+
 	};
-	makeCenter.prototype = {
+	MakeCenter.prototype = {
 		init : function() {
 
 		},
 	};
 
 	$.fn.extend({
-		makeCenter : makeCenter
+		makeCenter : function() {
+			// console.info(this)
+			new MakeCenter(settings, this);
+		}
 	});
 }();
