180b112106eedc73d88f0a03f3ee51bd1d1a2194
summetreechina <1095909657@qq.com>
2017-07-02 20:30:19 +0800

    红包

---
+++ Frontend/Canvas/js/blur.js
@@ -0,0 +1,88 @@
+-function(){
+	'use strict';
+
+	let imgW = $('#blur-div').width();
+	let imgH = $('#blur-div').height();
+
+	let canvas = document.querySelector('#canvas');
+	let context = canvas.getContext('2d');
+
+
+	canvas.width = imgW;
+	canvas.height = imgH;
+
+	let img = new Image();
+	let clippingRegion = { r:80 };
+	let timer = null;
+	img.src = "../images/e0f39278-f0f3-4e85-8eab-69ab725ac73d.jpg";
+	img.onload = function(e) {
+		initCanvas();
+		triggerEvent();
+	}
+
+	function initCanvas() {
+		clippingRegion = {
+			x : (Math.random()*(imgW - 2*clippingRegion.r)) + clippingRegion.r,
+			y : (Math.random()*(imgH - 2*clippingRegion.r)) + clippingRegion.r,
+			r : 80
+		};
+		draw(img, clippingRegion);
+	}
+
+	function draw(img, clippingRegion) {
+		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		context.save();	// save()和restore()存储|释放canvas的状态
+		setClippingRegion(clippingRegion);
+		context.drawImage(img, 0, 0);
+		context.restore();
+	}
+
+	function setClippingRegion(clippingRegion) {
+		context.beginPath();
+		context.arc(clippingRegion.x, clippingRegion.y, clippingRegion.r, 0, 2*Math.PI, false);
+		context.clip();
+		// 由此生成一个剪辑区域，后续画图只能在剪辑区域进行
+		// context.closePath();
+	}
+
+	function triggerEvent() {
+		$('#blur-div').on('click', ".button", function(e) {
+			e.preventDefault();
+			if (this.id == 'reset-button') {
+				reset();
+			}
+			if (this.id == 'show-button') {
+				show();
+			}
+			if (this.id == 'auto-button') {
+				// $(this).remove();
+				auto();
+			}
+		});
+	}
+
+	function reset() {
+		clearInterval(timer);
+		initCanvas();
+	}
+
+	function show() {
+		clearInterval(timer);
+		clippingRegion = { x:imgW/2, y:imgH/2, r:0 };
+		timer = setInterval(function(){
+			clippingRegion.r += 2;
+			draw(img, clippingRegion);
+			if (clippingRegion.r > 1000) {
+				clearInterval(timer);
+			}
+		}, 30);
+	}
+
+	function auto() {
+		clearInterval(timer);
+		timer = setInterval(function(){
+			initCanvas();
+		}, 1000);
+	}
+}();
--- Frontend/Canvas/js/makeCenter.js
+++ Frontend/Canvas/js/makeCenter.js
@@ -1,19 +1,19 @@
 +function(){
 	'use strict';
 
-	let MakeCenter = function(settings, dom) {
+	let MakeCenter = function(dom) {
 		let self = this;
 		self.dom = dom;
 		self.parent = dom.parent();
 		self.parent.css('position', 'relative');
 
 		self.offsetPosition = function() {
-			let topVal = (window.innerHeight - self.dom.height())/2;
+			let topVal = ($(window).height() - self.dom.height())/2;
 			topVal = (topVal > 0) ? topVal : 0;
 			self.dom.css({
-				background: '#b5b5b5',
+				background: '#eee',
 				position: 'relative',
-				top : topVal
+				top : topVal - 50
 			});
 		}
 		self.offsetPosition();
@@ -21,18 +21,19 @@
 		window.onresize = function(event) {
 			self.offsetPosition();
 		};
-
-		if (Object.keys(settings).length != 0) {
-			self.dom.offset({
-				top  : settings.offsetY,
-				left : settings.offsetX
-			});
-		}
 	};
 
 	$.fn.extend({
 		makeCenter : function() {
-			new MakeCenter(settings, this);
+			new MakeCenter(this);
 		}
 	});
 }();
+
+
+// if (Object.keys(settings).length != 0) {
+// 	self.dom.offset({
+// 		top  : settings.offsetY,
+// 		left : settings.offsetX
+// 	});
+// }
