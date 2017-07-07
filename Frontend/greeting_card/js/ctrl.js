322f0f26469bbdb61ade444dffae7f33998b01b8
summetreechina <1095909657@qq.com>
2017-07-07 16:02:30 +0800

    card

--- Frontend/greeting_card/js/ctrl.js
+++ Frontend/greeting_card/js/ctrl.js
@@ -3,6 +3,9 @@ window.onload = function() {
 
 	let music = document.querySelector('#music');
 	let audio = document.querySelector('audio');
+	let page1 = document.querySelector('#page1');
+	let page2 = document.querySelector('#page2');
+	let page3 = document.querySelector('#page3');
 
 	music.addEventListener("ended", function(e){
 		music.setAttribute("class", "");
@@ -20,6 +23,20 @@ window.onload = function() {
 			this.style.animationPlayState = "paused";
 		}
 	}, false);
+
+	page1.addEventListener('touchstart', function(e){
+
+		page1.style.display = 'none';
+		page2.style.display = 'block';
+		page3.style.display = 'block';
+		page3.style.top 	= "100%";
+
+		setTimeout(function(){
+			page2.setAttribute("class", "page fadeOut");
+			page3.setAttribute("class", "page fadeIn");
+		}, 5500);
+	}, false);
+
 }
 
 // music.onclick = function() {
