e216d28c92f3e85d763995a4250924d2b290d938
summetreechina <1095909657@qq.com>
2017-07-19 14:24:06 +0800

    valudate.js

--- Frontend/LF/sigin.js
+++ Frontend/LF/sigin.js
@@ -3,11 +3,29 @@
 
 	makeCenter();
 
+	closeBtn();
 
 	function makeCenter() {
 		let dom = $('#siginBox');
 		let offsetTop = ($(window).height() - dom.height()) / 4;
+
 		dom.css('margin-top', offsetTop + 'px');
-	}
+	};
+
+	function closeBtn() {
+		$('#siginBox .close-btn').on('click', function(){
+			$('#siginBox').hide();
+			$('#footer').addClass('animated fadeOut');
+		});
+	};
+
+	$('.js-link-effect').on('mouseover', function(e) {
+		e.preventDefault();
+		let linkEl = $(this);
+		linkEl.addClass('animated swing');
+		setTimeout(function(){
+			linkEl.removeClass('animated swing');
+		}, 1500);
+	});
 
 }();
