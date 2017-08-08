c1f56af7a4c7cbefa820d7b56e21dcfb56840e95
summetreechina <1095909657@qq.com>
2017-08-08 16:01:52 +0800

    color

--- Frontend/2048/main2048.js
+++ Frontend/2048/main2048.js
@@ -45,7 +45,7 @@ $(function(){
 
 		for (let i = 0; i < 4; i++) {
 			for (var j = 0; j < 4; j++) {
-				let number_cell = `<div class="number-cell" id="number-cell-${i}-${j}">0</div>`;
+				let number_cell = `<div class="number-cell" id="number-cell-${i}-${j}"></div>`;
 				$('#grid-container').append(number_cell);
 				let $numberCell = $(`#number-cell-${i}-${j}`);
 
@@ -61,11 +61,39 @@ $(function(){
 					$numberCell.css('left', getPosLeft(i, j));
 					$numberCell.css('background-color', getNumberBackgroundColor(board[i][j]));
 					$numberCell.css('color', getNumberColor(board[i][j]));
-					$numberCell.css(board[i][j]);
+					$numberCell.text(board[i][j]);
 				}
 			}
 		}
 	}
+
+	function getNumberBackgroundColor(number) {
+		switch(number) {
+			case 2: return '#eee4da'; break;
+			case 4: return '#ede0c8'; break;
+			case 8: return '#f2b179'; break;
+			case 16: return '#f59563'; break;
+			case 32: return '#f67c5f'; break;
+			case 64: return '#f65e3b'; break;
+			case 128: return '#edcf72'; break;
+			case 256: return '#edcc61'; break;
+			case 512: return '#9c0'; break;
+			case 1024: return '#33b5e5'; break;
+			case 2048: return '#09c'; break;
+			case 4096: return '#a6c'; break;
+			case 8192: return '#93c'; break;
+		}
+		return "black";
+	}
+
+	function getNumberColor(number) {
+		if (number <= 4) {
+			return '#776e65';
+		}
+		return 'white';
+	}
+
+
 });
 
 
