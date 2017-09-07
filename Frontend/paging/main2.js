d3cf63e8ef74b078251e4886fa8656427aa45856
summetreechina <1095909657@qq.com>
2017-09-07 21:35:13 +0800

    paging

--- Frontend/paging/main2.js
+++ Frontend/paging/main2.js
@@ -3,7 +3,7 @@
 
 	let json = {
 		'total_item' : 99,
-		'now_page' : 10,
+		'now_page' : 9,
 		'single_page_item' : 8,
 		'paging_container' : '#paging'
 	}
@@ -34,24 +34,56 @@
 
 	paging_container.on('click', 'a', function(event) {
 		event.preventDefault()
-		$(this).css({
-			'background-color' : '#337ab7',
-			'color' : '#d9edf7'
-		}).siblings('.current').removeClass('current');
-		now_page = $(this).html()
-		// $(this)
-		let next_page = now_page + 1
-		let prev_page = now_page - 1
-		console.log(now_page)
+		// $(this).css({
+		// 	'background-color' : '#337ab7',
+		// 	'color' : '#d9edf7'
+		// }).siblings('.current').removeClass('current');
+		$('#rend-area').empty()
+		now_page = parseInt($(this).html())
+		if ($(this).html() == '首页') {
+			now_page = 1
+			$('#frist-page').hide()
+			rend_paging(now_page)
+		}
+		if ($(this).html() == '末页') {
+			now_page = total_page
+			$('#last-page').hide()
+			rend_paging(now_page)
+		}
+		if ($(this).html() != '1' && $(this).html() != total_page) {
+			$('#last-page').show()
+			$('#frist-page').show()
+			rend_paging($(this).html())
+		}
 	});
+
 
 	// right_btn.style.visibility = 'hidden'
 
-	
+	function rend_paging(now_page) {
+		let html_tmp = ``
+		if ((now_page*1 - 5) <= 0) {
+			for (let i = 1; i <= 10; i++) {
+				if (i == now_page) {
+					html_tmp += `<a href="#${i}" class="current">${i}</a>`
+				} else {
+					html_tmp += `<a href="#${i}">${i}</a>`
+				}
+			}
+		}
 
-
-
-console.log(pages)
+		if ((now_page*1 - 5) > 0 && (now_page*1 + 4) <= total_page) {
+			for (let i = (now_page*1 - 5); i <= (now_page*1 + 4); i++) {
+				console.log(i)
+				if (i == now_page) {
+					html_tmp += `<a href="#${i}" class="current">${i}</a>`
+				} else {
+					html_tmp += `<a href="#${i}">${i}</a>`
+				}
+			}
+		}
+		$('#rend-area').empty().append(html_tmp)
+	}
 }
 
 
