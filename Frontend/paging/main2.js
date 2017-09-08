f0fa33bb66899e847b7c73dfe89e3358a3d91b2d
summetreechina <1095909657@qq.com>
2017-09-08 13:14:18 +0800

    paging

--- Frontend/paging/main2.js
+++ Frontend/paging/main2.js
@@ -3,7 +3,7 @@
 
 	let json = {
 		'total_item' : 99,
-		'now_page' : 9,
+		'now_page' : 13,
 		'single_page_item' : 8,
 		'paging_container' : '#paging'
 	}
@@ -17,51 +17,49 @@
 	let last_page = $('#last-page')
 	let pages = $('#paging>a')
 
-	pages.each(function(index, el) {
-		if ($(el).hasClass('current')) {
-			$(this).removeClass('current')
-		}
-		if ($(el).index() == json.now_page) {
-			$(this).addClass('current')
-		}
-		if (json.now_page == 1) {
-			frist_page.hide();
-		}
-		if (json.now_page == 10) {
-			last_page.hide();
-		}
-	});
+	rend_paging(12)
 
-	paging_container.on('click', 'a', function(event) {
-		event.preventDefault()
-		// $(this).css({
-		// 	'background-color' : '#337ab7',
-		// 	'color' : '#d9edf7'
-		// }).siblings('.current').removeClass('current');
-		$('#rend-area').empty()
-		now_page = parseInt($(this).html())
-		if ($(this).html() == '首页') {
+	paging_container.on('click', 'a', function(e) {
+		e.preventDefault()
+		now_page = $(this).html()
+		if ($(this).html() === '首页') {
 			now_page = 1
-			$('#frist-page').hide()
-			rend_paging(now_page)
-		}
-		if ($(this).html() == '末页') {
+			// $('#frist-page').fadeOut()
+			// $('#last-page').fadeIn()
+			rend_paging(1)
+		} else if ($(this).html() === '末页') {
 			now_page = total_page
-			$('#last-page').hide()
+			// $('#last-page').fadeOut()
+			// $('#frist-page').fadeIn()
 			rend_paging(now_page)
+		} else {
+			if ($(this).html() == '1') {
+				rend_paging(1)
+			}
+			if ($(this).html() == total_page) {
+				rend_paging(total_page)
+			}
+			if ($(this).html() != '1' && $(this).html() != total_page) {
+				// $('#last-page').show()
+				// $('#frist-page').show()
+				rend_paging($(this).html())
+			}
 		}
-		if ($(this).html() != '1' && $(this).html() != total_page) {
-			$('#last-page').show()
-			$('#frist-page').show()
-			rend_paging($(this).html())
-		}
+		// console.log(this)
 	});
 
 
-	// right_btn.style.visibility = 'hidden'
 
 	function rend_paging(now_page) {
 		let html_tmp = ``
+		if (now_page == 1) {
+			$('#frist-page').hide()
+			$('#last-page').fadeIn()
+		}
+		if (now_page == total_page) {
+			$('#last-page').hide()
+			$('#frist-page').fadeIn()
+		}
 		if ((now_page*1 - 5) <= 0) {
 			for (let i = 1; i <= 10; i++) {
 				if (i == now_page) {
@@ -72,9 +70,18 @@
 			}
 		}
 
+		if ((now_page*1 + 4) > total_page) {
+			for (let i = total_page - 10; i <= total_page; i++) {
+				if (i == now_page) {
+					html_tmp += `<a href="#${i}" class="current">${i}</a>`
+				} else {
+					html_tmp += `<a href="#${i}">${i}</a>`
+				}
+			}
+		}
+
 		if ((now_page*1 - 5) > 0 && (now_page*1 + 4) <= total_page) {
 			for (let i = (now_page*1 - 5); i <= (now_page*1 + 4); i++) {
-				console.log(i)
 				if (i == now_page) {
 					html_tmp += `<a href="#${i}" class="current">${i}</a>`
 				} else {
@@ -84,6 +91,7 @@
 		}
 		$('#rend-area').empty().append(html_tmp)
 	}
+
 }
 
 
