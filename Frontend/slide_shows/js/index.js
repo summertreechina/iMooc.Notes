753ff868dd61dece5b18013cb63c9e96bac67754
summetreechina <1095909657@qq.com>
2017-09-12 00:02:37 +0800

    pic_panel_shows

---
+++ Frontend/slide_shows/js/index.js
@@ -0,0 +1,42 @@
+{
+	let jsonData = {
+		img1 : {
+			title : 'nnn',
+			src : 'img/index/1.jpg'
+		},
+		img2 : {
+			title : 'aaaaaaaa',
+			src : 'img/index/2.jpg'
+		},
+		img3 : {
+			title : 'bbbbbbbb',
+			src : 'img/index/3.jpg'
+		},
+		img4 : {
+			title : 'jjjjjjjj',
+			src : 'img/index/4.jpg'
+		},
+		img5 : {
+			title : 'ggggggggg',
+			src : 'img/index/5.jpg'
+		}
+	}
+
+	let panel_shows = document.querySelector('#pic_panel_shows')
+	let panel_main = panel_shows.querySelector('.p_p_main')
+	let active_dom = panel_shows.querySelector('.active')
+
+	let i_zindex = 1;
+	let i_now = 0;
+
+	for (let i = 0; i < .length; i++) {
+		[i]
+	}
+
+
+
+
+
+
+	
+}
