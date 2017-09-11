b852df56f4a2514ed31f35c06e3abe6389923e15
summetreechina <1095909657@qq.com>
2017-09-12 00:52:39 +0800

    onclick

--- Frontend/slide_shows/js/index.js
+++ Frontend/slide_shows/js/index.js
@@ -1,4 +1,5 @@
 {
+
 	let jsonData = {
 		img1 : {
 			title : 'nnn',
@@ -23,15 +24,24 @@
 	}
 
 	let panel_shows = document.querySelector('#pic_panel_shows')
-	let panel_main = panel_shows.querySelector('.p_p_main')
-	let active_dom = panel_shows.querySelector('.active')
+	let large_pic_box = panel_shows.querySelector('.large_pic_wrap')
+	let min_pic_box = panel_shows.querySelector('#min_pic_wrap')
+	let active_el = panel_shows.querySelector('.active')
+	let large_pics = large_pic_box.querySelectorAll('.large_pic')
+	let min_pics = min_pic_box.querySelectorAll('li')
 
+	// console.log(panel_shows, large_pic_box, min_pic_box, active_el, large_pics, min_pics)
 	let i_zindex = 1;
 	let i_now = 0;
 
-	for (let i = 0; i < .length; i++) {
-		[i]
-	}
+	min_pic_box.onclick = function() {
+		console.log(this)
+	} 
+
+
+	// for (let i = 0; i < .length; i++) {
+	// 	[i]
+	// }
 
 
 
